858941e781c0bbf549ebcc80d3896f79168974f9
victorpawlek <pawlek.v03@htlwienwest.at>
2021-10-20 14:12:52 +0200

    fertig

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,33 +2,22 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="grey"
       dark
     >
-      <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          alt="Logo"
+          class="shrink mt-10"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+          src="logo.png"
+          width="130"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+<v-toolbar-title>Movies</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://www.imdb.com/movies-in-theaters/"
         target="_blank"
         text
       >
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,66 @@
 <template>
-  <hello-world />
+  <div style="margin: 20px">
+    <v-alert color="grey" style="margin-left:100px">Rent selected movies: {{selected.map(el=>el.title).join(',')}}</v-alert>
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="movies"
+      item-key="title"
+      :search="search"
+      show-select
+      items-per-page="6"
+      class="elevation-1"
+      :footer-props="{ itemsPerPageOptions: [6, 12, 18] }"
+    >
+    <template v-slot:top>
+        <v-text-field
+          v-model="search"
+          label="Search"
+          class="mx-4"
+        ></v-text-field>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import axios from 'axios';
 
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
-    components: {
-      HelloWorld,
+  components: {},
+  data() {
+    return {
+      search:'',
+      movies: [],
+      selected: [],
+      headers: [
+        {
+          text: 'Title',
+          align: 'start',
+          value: 'title',
+          class: 'blue-grey lighten-5',
+        },
+        { text: 'Director', value: 'director', class: 'blue-grey lighten-5' },
+        { text: 'Genre', value: 'main_genre', class: 'blue-grey lighten-5' },
+        { text: 'Year', value: 'year', width: 100, class: 'blue-grey lighten-5' },
+        { text: 'Rated', value: 'rated', width: 100, class: 'blue-grey lighten-5' },
+        { text: 'Plot', value: 'plot' },
+      ],
+    };
+  },
+  created() {
+    this.getMovies();
+  },
+  methods: {
+    async getMovies() {
+      const a = await axios({
+        method: 'GET',
+        url: 'http://localhost:3000/movies',
+      });
+      this.movies = a.data.data;
     },
-  }
+  },
+};
 </script>
